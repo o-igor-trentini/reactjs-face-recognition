b91bb1d822dac7ad1a3785605ea0e5ca6e5b6bb9
Igor Trentini <igortrentini.2004@gmail.com>
2024-02-20 18:28:59 -0300

    fix: Corrigir reconhecimento facial com base url diferente

--- src/lib/myfaceapi/index.ts
+++ src/lib/myfaceapi/index.ts
@@ -1,6 +1,6 @@
 import * as faceapi from "face-api.js";
 
-const MODEL_URL = '/vendor/models';
+const MODEL_URL = import.meta.env.BASE_URL + 'vendor/models';
 
 const showConfig: ShowProps = {
     detectionBox: true,
